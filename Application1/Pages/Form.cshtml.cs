f2a4c59fddfa6d4573add4d737ba9d5239647587
Vimal Solanki <vimal.solanki@hotmail.co.uk>
2022-05-26 15:41:08 +0100

    Form page

--- Application1/Pages/Form.cshtml.cs
+++ Application1/Pages/Form.cshtml.cs
@@ -5,26 +5,9 @@ namespace Application1.Pages
 {
     public class Index1Model : PageModel
     {
-
-
-        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
-        public Index1Model(Microsoft.Extensions.Hosting.IHostingEnvironment environment)
-        {
-            _environment = (Microsoft.AspNetCore.Hosting.IHostingEnvironment?)environment;
-        }
-        [BindProperty]
-        public IFormFile Upload { get; set; }
-        public async Task OnPostAsync()
-        {
-            var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
-            using (var fileStream = new FileStream(file, FileMode.Create))
-            {
-                await Upload.CopyToAsync(fileStream);
-            }
-        }
         public void OnGet()
         {
-
+            var emailAddress = Request.Form["emailaddress"];
         }
     }
 }
